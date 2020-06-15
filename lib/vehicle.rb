3a7a20fbfda316fada5171a4e91f54f49946b3f8
Alain Saint-Dic <alains214@gmail.com>
2020-06-15 01:40:59 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,4 +1,16 @@
-class Vehicle
+class Vehicleclass Car 
+  
+  attr_accessor :wheel_size, :wheel_number
+   
+    def initialize(wheel_size, wheel_number)
+      @wheel_size = wheel_size
+      @wheel_number = wheel_number
+    end 
+    
+    def go 
+      "vrrrrrrrooom!"
+    end
+
   
   
 
